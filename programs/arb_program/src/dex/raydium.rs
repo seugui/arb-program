58359add4c559708814d04250f103f1e135474e8
sebastian.eugui <sebastian.eugui@vodafone.com>
2024-12-09 18:23:41 +0100

    Update

--- programs/arb_program/src/dex/raydium.rs
+++ programs/arb_program/src/dex/raydium.rs
@@ -1,22 +1,22 @@
 use anchor_lang::prelude::*;
-use solana_program::instruction::Instruction;
+use solana_program::instruction::{AccountMeta, Instruction};
 use solana_program::program::invoke;
-use anchor_spl::token::{TokenAccount, Token};
-use crate::Swap;
 
-pub fn handle_swap(ctx: Context<Swap>, amount_in: u64) -> Result<()> {
+pub fn handle_swap(ctx: Context<crate::Swap>, amount_in: u64) -> Result<()> {
+    let accounts = vec![
+        AccountMeta::new_readonly(*ctx.accounts.amm_id.key, false),
+        AccountMeta::new_readonly(*ctx.accounts.amm_authority.key, false),
+        AccountMeta::new(*ctx.accounts.pool_coin_token_account.to_account_info().key, false),
+        AccountMeta::new(*ctx.accounts.pool_pc_token_account.to_account_info().key, false),
+        AccountMeta::new(*ctx.accounts.user_source_token_account.to_account_info().key, false),
+        AccountMeta::new(*ctx.accounts.user_destination_token_account.to_account_info().key, false),
+        AccountMeta::new_readonly(*ctx.accounts.user_wallet.key, true),
+        AccountMeta::new_readonly(*ctx.accounts.token_program.key, false),
+    ];
+
     let swap_instruction = Instruction {
         program_id: ctx.accounts.amm_id.key(),
-        accounts: vec![
-            ctx.accounts.amm_id.to_account_info(),
-            ctx.accounts.amm_authority.to_account_info(),
-            ctx.accounts.pool_coin_token_account.to_account_info(),
-            ctx.accounts.pool_pc_token_account.to_account_info(),
-            ctx.accounts.user_source_token_account.to_account_info(),
-            ctx.accounts.user_destination_token_account.to_account_info(),
-            ctx.accounts.user_wallet.to_account_info(),
-            ctx.accounts.token_program.to_account_info(),
-        ],
+        accounts,
         data: build_swap_data(amount_in),
     };
 
--- programs/arb_program/src/lib.rs
+++ programs/arb_program/src/lib.rs
@@ -1,23 +1,24 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{TokenAccount, Token};
-use crate::dex::raydium;
 
-declare_id!("Rw9S416EA9rc6CA8UGazHSg4XZW3FvyoUE58akafcT9");
+mod dex; // Registra el folder dex como un módulo
+
+declare_id!("B2XEUf2nBJHwQNbgNhdbF6eUkspJZx2ekNfbFc4UvUU");
 
 #[program]
 pub mod arb_program {
     use super::*;
 
     pub fn swap(ctx: Context<Swap>, amount_in: u64) -> Result<()> {
-        raydium::handle_swap(ctx, amount_in)
+        dex::raydium::handle_swap(ctx, amount_in)
     }
 }
 
 #[derive(Accounts)]
 pub struct Swap<'info> {
-    /// CHECK: Validación en tiempo de ejecución
+    /// CHECK: Esta es la cuenta del AMM en Raydium, no necesita validación adicional.
     pub amm_id: AccountInfo<'info>,
-    /// CHECK: Validación en tiempo de ejecución
+    /// CHECK: La autoridad del AMM no necesita validación adicional porque es parte del contrato Raydium.
     pub amm_authority: AccountInfo<'info>,
     #[account(mut)]
     pub pool_coin_token_account: Account<'info, TokenAccount>,
@@ -27,6 +28,7 @@ pub struct Swap<'info> {
     pub user_source_token_account: Account<'info, TokenAccount>,
     #[account(mut)]
     pub user_destination_token_account: Account<'info, TokenAccount>,
+    /// CHECK: Esta cuenta pertenece al usuario final y no requiere validación adicional.
     #[account(signer)]
     pub user_wallet: AccountInfo<'info>,
     pub token_program: Program<'info, Token>,
