cd29e062d4c6d197cb01b4820a489b6271b2bfd2
sebastian.eugui <sebastian.eugui@vodafone.com>
2024-12-10 11:47:11 +0100

    Update

--- set_env.sh
+++ set_env.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ "$1" == "localnet" ]]; then
-  sed -i '' 's|cluster = .*|cluster = "http://127.0.0.1:8899"|' Anchor.toml
-  solana config set --url http://127.0.0.1:8899
+  sed -i '' 's|cluster = .*|cluster = "localnet"|' Anchor.toml
+  solana config set --url http://127.0.0.1:8900
   echo "Switched to Localnet"
 
 elif [[ "$1" == "devnet" ]]; then
-  sed -i '' 's|cluster = .*|cluster = "https://api.devnet.solana.com"|' Anchor.toml
+  sed -i '' 's|cluster = .*|cluster = "devnet"|' Anchor.toml
   solana config set --url https://api.devnet.solana.com
   echo "Switched to Devnet"
 
 elif [[ "$1" == "mainnet" ]]; then
-  sed -i '' 's|cluster = .*|cluster = "https://api.mainnet-beta.solana.com"|' Anchor.toml
+  sed -i '' 's|cluster = .*|cluster = "mainnet"|' Anchor.toml
   solana config set --url https://api.mainnet-beta.solana.com
   echo "Switched to Mainnet"
 
